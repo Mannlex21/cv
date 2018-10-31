25d75228bfcb655a30fb948f1d4a73936581e3ad
mamurillo <mn.murillo@mavi.mx>
2018-10-30 18:50:33 -0600

    first build

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,6 @@ const path = require("path");
 module.exports = {
     outputDir: path.resolve(__dirname, "./docs"),
     baseUrl: process.env.NODE_ENV === 'production'
-    ? '/prueba-unexpect/'
+    ? '/cv/'
     : './'
 }
